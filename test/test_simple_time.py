f8a781f17fc38c21dc03df3c1b9ecb87705facdf
J.Easter <295140325@qq.com>
2019-07-27 16:53:14 +0800

    simple_time: 将 simple_time.py 做成可以被其他 project 引用的类库

--- test/test_simple_time.py
+++ test/test_simple_time.py
@@ -4,24 +4,24 @@
 
 import unittest
 
-import simple_time
+from simple_time import st
 
 
 class TestSimpleTime(unittest.TestCase):
 
     def test_is_valid_datetime(self):
-        self.assertTrue(simple_time.is_valid_datetime('2019-09-01', '%Y-%m-%d'))
-        self.assertFalse(simple_time.is_valid_datetime('2019-09-31', '%Y-%m-%d'))
-        self.assertFalse(simple_time.is_valid_datetime('2019-09-01', '%Y-%m'))
+        self.assertTrue(st.is_valid_datetime('2019-09-01', '%Y-%m-%d'))
+        self.assertFalse(st.is_valid_datetime('2019-09-31', '%Y-%m-%d'))
+        self.assertFalse(st.is_valid_datetime('2019-09-01', '%Y-%m'))
 
     def test_compare_date(self):
-        self.assertEqual(simple_time.compare_datetime('2019-09-01', '%Y-%m-%d',
+        self.assertEqual(st.compare_datetime('2019-09-01', '%Y-%m-%d',
                                                   '2019-10-02', '%Y-%m-%d'), -1)
-        self.assertEqual(simple_time.compare_datetime('2019-09-01', '%Y-%m-%d',
+        self.assertEqual(st.compare_datetime('2019-09-01', '%Y-%m-%d',
                                                   '2019-09-01', '%Y-%m-%d'), 0)
-        self.assertEqual(simple_time.compare_datetime('2019-11-01', '%Y-%m-%d',
+        self.assertEqual(st.compare_datetime('2019-11-01', '%Y-%m-%d',
                                                   '2019-10-02', '%Y-%m-%d'), 1)
-        self.assertEqual(simple_time.compare_datetime('2019-11', '%Y-%m',
+        self.assertEqual(st.compare_datetime('2019-11', '%Y-%m',
                                                   '2019-10', '%Y-%m'), 1)
-        self.assertEqual(simple_time.compare_datetime('2019-11', '%Y-%m',
+        self.assertEqual(st.compare_datetime('2019-11', '%Y-%m',
                                                   '2019-10-15', '%Y-%m-%d'), 1)
