6e8005fb7e6fa71e0469861b4c9469f7d6401677
J.Easter <295140325@qq.com>
2019-07-27 16:11:33 +0800

    test_simple_time: 修改单元测试的函数名，以匹配原本函数的名字
    Signed-off-by: J.Easter <295140325@qq.com>

--- test/test_simple_time.py
+++ test/test_simple_time.py
@@ -9,7 +9,7 @@ import simple_time
 
 class TestSimpleTime(unittest.TestCase):
 
-    def test_validate_date(self):
+    def test_is_valid_datetime(self):
         self.assertTrue(simple_time.is_valid_datetime('2019-09-01', '%Y-%m-%d'))
         self.assertFalse(simple_time.is_valid_datetime('2019-09-31', '%Y-%m-%d'))
         self.assertFalse(simple_time.is_valid_datetime('2019-09-01', '%Y-%m'))
